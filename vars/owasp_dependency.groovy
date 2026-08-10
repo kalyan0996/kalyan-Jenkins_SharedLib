11b36c9e5ab42d99a7c4c822f5b1641ab2a8fadd
JARPULA KALYAN <kalyanjarupla2002@gmail.com>
2026-08-10 16:20:47 +0530

    Update dependencyCheck arguments to include 'noupdate'

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,6 +1,6 @@
 def call() {
     dependencyCheck(
-        additionalArguments: '--scan ./ --data /var/lib/jenkins/dependency-check-data',
+        additionalArguments: '--scan ./ --noupdate --data /var/lib/jenkins/dependency-check-data',
         odcInstallation: 'OWASP'
     )
     dependencyCheckPublisher(
