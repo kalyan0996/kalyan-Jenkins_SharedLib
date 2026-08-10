2df7b1eb3cd521838895cc3cc0f38cba05106754
JARPULA KALYAN <kalyanjarupla2002@gmail.com>
2026-08-10 16:33:26 +0530

    Disable NVD API in OWASP dependency check

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,9 +1,10 @@
 def call() {
     dependencyCheck(
-        additionalArguments: '--scan ./ --noupdate --data /var/lib/jenkins/dependency-check-data --format ALL',
+        additionalArguments: '--scan ./ --noupdate --data /var/lib/jenkins/dependency-check-data --disableNvdApi --format ALL',
         odcInstallation: 'OWASP'
     )
     dependencyCheckPublisher(
         pattern: '**/dependency-check-report.xml'
     )
 }
+EOF
