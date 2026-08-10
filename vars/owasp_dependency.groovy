969093e9c49e442e3afaf8be605d04bb3bfc51b7
JARPULA KALYAN <kalyanjarupla2002@gmail.com>
2026-08-10 16:17:16 +0530

    Simplify dependency check by removing API key
    Removed NVD API key usage from dependency check.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,10 +1,8 @@
 def call() {
-    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
-        dependencyCheck(
-            additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY} --data /var/lib/jenkins/dependency-check-data",
-            odcInstallation: 'OWASP'
-        )
-    }
+    dependencyCheck(
+        additionalArguments: '--scan ./ --data /var/lib/jenkins/dependency-check-data',
+        odcInstallation: 'OWASP'
+    )
     dependencyCheckPublisher(
         pattern: '**/dependency-check-report.xml'
     )
