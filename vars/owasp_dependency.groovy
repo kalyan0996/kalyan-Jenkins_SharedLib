ba203542560018a3be87ed47b49da9c0ff47679f
JARPULA KALYAN <kalyanjarupla2002@gmail.com>
2026-08-10 16:15:30 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,9 +1,10 @@
 def call() {
-    dependencyCheck(
-        additionalArguments: '--scan ./ --noupdate',
-        odcInstallation: 'OWASP'
-    )
-
+    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+        dependencyCheck(
+            additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY} --data /var/lib/jenkins/dependency-check-data",
+            odcInstallation: 'OWASP'
+        )
+    }
     dependencyCheckPublisher(
         pattern: '**/dependency-check-report.xml'
     )
